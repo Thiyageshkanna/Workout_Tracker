410f61b8ec412c09a7b28509197076e4baeeb5ae
thiyagesh kanna <thiyagesh14092000@gmail.com>
2021-03-06 22:52:31 +0530

    Map and Form Over

--- script.js
+++ script.js
@@ -10,3 +10,68 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+// |---------------------------------------------------------------------------------------|
+
+// making Global variable to access everywhere
+let map, mapEvent;
+// LeafLet API
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`${latitude} and ${longitude} `);
+
+      const cords = [latitude, longitude];
+      map = L.map("map").setView(cords, 9);
+
+      L.tileLayer(
+        "https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png",
+        {
+          maxZoom: 20,
+          attribution:
+            '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+        }
+      ).addTo(map);
+
+      map.on("click", (mapE) => {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+      });
+    },
+    () => alert("click allow to access your location"),
+    {
+      enableHighAccuracy: true,
+    }
+  );
+}
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value =
+    "";
+  inputDistance.focus();
+
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+});
+
+inputType.addEventListener("change", () => {
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  inputType.focus();
+});
